4efb969493e410ca468bc869a95e6146811f88c8
Anna Carolina <ac.luna95@gmail.com>
2020-02-06 12:00:25 -0400

    Created tables authors and books

--- olist_library/core/models.py
+++ olist_library/core/models.py
@@ -1,3 +1,40 @@
-from django.db import models
+from djongo import models
+from pymongo import MongoClient
+from config import mongo_port, mongo_host
 
-# Create your models here.
+MONGO_HOST = f'mongodb://{mongo_host}:{mongo_port}/test'
+
+
+class Authors(models.Model):
+    '''
+    This authors represent all imported authors by csv
+    '''
+    authors_name = models.CharField(max_length=50, blank=False)
+
+    def get_client(self):
+        self.db = MongoClient(host=MONGO_HOST)
+        client = self.db.library_books.core_filter
+
+        return client
+
+
+class Books(models.Model):
+    '''
+    This books represent all books created
+    '''
+    book_name = models.CharField(max_length=70, blank=False)
+    book_edition = models.CharField(max_length=5, blank=False)
+    book_year = models.CharField(max_length=4, blank=False)
+    book_author = models.ForeignKey(Authors, on_delete=models.CASCADE, related_name='book_author', blank=False)
+
+    def get_client(self):
+        self.db = MongoClient(host=MONGO_HOST)
+        client = self.db.library_books.core_filter
+
+        return client
+
+    def get_book(self, str_book):
+        client = self.get_client()
+        result = client.find_one({'id': str_book})
+
+        return result['book_name']
