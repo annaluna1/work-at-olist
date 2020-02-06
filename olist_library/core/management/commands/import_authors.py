ad05834d6019c6b9bf05d4362e7c0209025c3072
Anna Carolina <ac.luna95@gmail.com>
2020-02-06 18:55:46 -0400

    Created a command management import_authors

--- olist_library/core/management/commands/import_authors.py
+++ olist_library/core/management/commands/import_authors.py
@@ -1,5 +1,6 @@
-from core.models import Author
-from django.core.management.base import BaseCommand,CommandError
+from core.models import Authors
+from django.core.management.base import BaseCommand, CommandError
+from pymongo import MongoClient
 import csv
 
 
@@ -8,5 +9,24 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('authors', type=str, help='Indicate the CSV file to be imported')
+
+    def handle(self, *args, **options):
+        try:
+            file = open(options['authors'])
+            open_file = csv.reader(file)
+            client = Authors.get_client(self)
+            authors = []
+            for line in open_file:
+                result = client.find_one({'authors_name': line})
+                if result:
+                    next
+                else:
+                    authors.append(line)
+            client.insert_many([{'authors_name': name} for name in authors])
+            client.delete_one({'authors_name': 'name'})
+        except:
+            self.stdout.write('The file are not updated on database, check the format file ou if the file are created')
+        else:
+            self.stdout.write('The file was imported')
 
 
