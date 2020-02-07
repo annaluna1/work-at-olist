5bc7089b816440855ae61a98c9dc7f4932f2b4c9
Anna Carolina <ac.luna95@gmail.com>
2020-02-07 19:55:36 -0400

    add Authors to API

--- olist_library/core/views.py
+++ olist_library/core/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets, generics, pagination, mixins
+
+from .models import Authors, Books
+from .serializers import BooksSerializer, AuthorsSerializer
+
 
 # Create your views here.
+class Authors(mixins.ListModelMixin,viewsets.GenericViewSet):
+    queryset = Authors.objects.all()
+    serializer_class = AuthorsSerializer
+    pagination_class = pagination.LimitOffsetPagination
