d96a60cc2c68410f37fe2f754e844816ce981e39
Anna Carolina <ac.luna95@gmail.com>
2020-02-07 19:50:10 -0400

    Relation urls.py to core.views

--- olist_library/olist_library/urls.py
+++ olist_library/olist_library/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from core import views
+
+router = routers.DefaultRouter()
+router.register(r'books', views.Books)
+router.register(r'authors', views.Authors)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
