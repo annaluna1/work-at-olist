c9e048740bb5a898b42060b187f48ba13fc2201c
Anna Carolina <ac.luna95@gmail.com>
2020-02-07 21:30:32 -0400

    Authors views to paginated and filter by name

--- olist_library/core/views.py
+++ olist_library/core/views.py
@@ -1,11 +1,32 @@
-from rest_framework import viewsets, generics, pagination, mixins
+from rest_framework import viewsets, generics, pagination, mixins, filters
+from django_filters import rest_framework as filters
 
 from .models import Authors, Books
 from .serializers import BooksSerializer, AuthorsSerializer
 
 
 # Create your views here.
-class Authors(mixins.ListModelMixin,viewsets.GenericViewSet):
+
+class AuthorFilter(filters.FilterSet):
+    name = filters.CharFilter(field_name="authors_name")
+
+    class Meta:
+        model = Authors
+        fields = ['name']
+
+
+class Authors(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
     queryset = Authors.objects.all()
     serializer_class = AuthorsSerializer
     pagination_class = pagination.LimitOffsetPagination
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = AuthorFilter
+
+
+class Books(viewsets.ModelViewSet):
+    queryset = Books.objects.all()
+    serializer_class = BooksSerializer
+    pagination_class = pagination.LimitOffsetPagination
+
+
+
