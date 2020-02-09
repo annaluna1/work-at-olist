83ad8bb03eb7465367cadc61e0c9f88e9296373c
Anna Carolina <ac.luna95@gmail.com>
2020-02-09 16:16:45 -0400

    Heroku settings

--- olist_library/core/urls.py
+++ olist_library/core/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from .views import Authors, Books
 
 urlpatterns = [
-    path('authors/', Authors.as_view(), name='Authors'),
+    path('', Authors.as_view(), name='Authors'),
     path('books/<pk>', Books.as_view(), name='Books'),
 ]
